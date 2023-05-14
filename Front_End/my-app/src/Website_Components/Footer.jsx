47602695073d81d4d3686170f67bf230534ec11a
HafizHammad123 <127588251+HafizHammad123@users.noreply.github.com>
2023-05-14 22:23:36 +0500

    Update night (14-05-2023)

--- Front_End/my-app/src/Website_Components/Footer.jsx
+++ Front_End/my-app/src/Website_Components/Footer.jsx
@@ -4,9 +4,8 @@ export default function Footer() {
     return <>
         <footer id="foot">
 
+            
             <div id="top">
-
-
                 <div class="fotter_sections">
                     <h3>About us</h3>
                     <p>Hello Bloggers we are looking for creative writers who can write for our blogging website and share your thoughts on differnt topics according to your desires</p>
@@ -19,7 +18,7 @@ export default function Footer() {
                     <ul class="adress">
                         <li>
                             <span><i class="fa-regular fa-envelope"></i></span>
-                            <span>HafizMuhammadHammad50@gmail.com</span>
+                            <span>mh176316@gmail.com</span>
                         </li>
                         <li>
                             <span><i class="fa-solid fa-phone"></i></span>
@@ -38,50 +37,43 @@ export default function Footer() {
 
                 <div class="fotter_sections">
                     <h3>Share your post</h3>
-                    <ul  className="share">
+                    <ul className="share">
                         <li><i class="fa-brands fa-facebook"></i></li>
                         <li><i class="fa-brands fa-twitter"></i></li>
                         <li><i class="fa-brands fa-whatsapp"></i></li>
                         <li><i class="fa-brands fa-instagram"></i></li>
                     </ul>
                 </div>
-
-
             </div>
 
 
             <div id="bottom">
-                <div class="fotter_section1">
-                    Copyright 
-                    <i class="fa-solid fa-copyright"></i>
-                     2012 - 2023 TermsFeed®. All rights reserved.
-                     </div>
-
-
+                <div class="fotter_section1" id="copyright">
+                    Copyright © 2012 - 2023 TermsFeed®. All rights reserved.
+                </div>
                 <div class="fotter_section1">
                     <ul className="footer_navbar">
-<li>
-    <span><i class="fa-solid fa-house"></i></span>
-    <span>Home</span>
-    </li>
+                        <li>
+                            <span><i class="fa-solid fa-house"></i></span>
+                            <span>Home</span>
+                        </li>
 
 
-<li>
-    <span><i class="fa-solid fa-blog"></i></span>
-    <span>Blog</span>
-</li>
+                        <li>
+                            <span><i class="fa-solid fa-blog"></i></span>
+                            <span>Blog</span>
+                        </li>
 
-<li>
-    <span><i class="fa-solid fa-phone"></i></span>
-    <span>Contact us</span>
-</li>
+                        <li>
+                            <span><i class="fa-solid fa-phone"></i></span>
+                            <span>Contact us</span>
+                        </li>
 
 
                     </ul>
-                    
-                    
-                    </div>
 
+
+                </div>
             </div >
         </footer>
     </>
