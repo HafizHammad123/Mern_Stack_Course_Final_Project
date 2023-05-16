12ee6840ee30c37b23f5842e2cd308e557808705
Abdul-Qadir123 <129989242+Abdul-Qadir123@users.noreply.github.com>
2023-05-16 12:53:23 +0500

    new thing updates

--- Front_End/my-app/src/Web_App_Pages/MyBlogs.jsx
+++ Front_End/my-app/src/Web_App_Pages/MyBlogs.jsx
@@ -48,7 +48,7 @@ export default function MyBlogs() {
                             <Box sx={style}>
                             <Typography variant="h5" display={"flex"} justifyContent={"center"} border={1}>Blogs Post Form</Typography>
                             <div>
-                                form abdul qadir
+                                form abdul qadir khan jatoi
                             </div>
                             </Box>
                         </Modal>
