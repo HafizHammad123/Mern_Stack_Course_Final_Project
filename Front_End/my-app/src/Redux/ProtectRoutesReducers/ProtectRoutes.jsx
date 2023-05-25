37a1448e74d2be0f22b36befa25f70f33cfde551
HafizHammad123 <127588251+HafizHammad123@users.noreply.github.com>
2023-05-25 21:05:49 +0500

    new update

--- Front_End/my-app/src/Redux/ProtectRoutesReducers/ProtectRoutes.jsx
+++ Front_End/my-app/src/Redux/ProtectRoutesReducers/ProtectRoutes.jsx
@@ -2,33 +2,27 @@ import { createSlice } from "@reduxjs/toolkit";
 export const Protect_Routes = createSlice({
     name: "Protect_Routes",
     initialState: {
-        WebsiteRoutes: true,
+      
         DashboardRoutes:localStorage.getItem('SecretKey'),
-        NotSignin: true,
-        LoginProper: false
+        
 
     },
     reducers:
     {
         LoggedIn: (state,action) => {
             state.DashboardRoutes = action.payload;
-            state.NotSignin = false;
-            state.LoginProper = true
+            
 
 
         },
         LoggedOut: (state) => {
             state.DashboardRoutes = false
-            state.NotSignin = true;
-            state.LoginProper = false
-            state.WebsiteRoutes = true
+         
         },
-        GotoDashBoard: (state) => {
-            state.WebsiteRoutes = false
-        }
+       
 
     }
 })
 
-export const { LoggedIn, LoggedOut,GotoDashBoard } = Protect_Routes.actions;
+export const { LoggedIn, LoggedOut } = Protect_Routes.actions;
 export default Protect_Routes.reducer
